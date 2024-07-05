4acb260e180c9c236cbb52934267fefac6b7e269
Ninja Left <ninja.notleft@proton.me>
2024-07-05 11:53:52 +0000

    Included config ui files
    Signed-off-by: Ninja Left <ninja.notleft@proton.me>

--- modules/compile_fix.sh
+++ modules/compile_fix.sh
@@ -2,7 +2,9 @@
 
 INPUT_FILE='main_ui.py'
 INPUT_FILE2='bf_ui.py'
+INPUT_FILE3='config_ui.py'
 
 sed "s/import resources_rc//" $INPUT_FILE >tmp && mv tmp $INPUT_FILE && echo "Fixed $INPUT_FILE"
 sed "s/import resources_rc//" $INPUT_FILE2 >tmp && mv tmp $INPUT_FILE2 && echo "Fixed $INPUT_FILE2"
+sed "s/import resources_rc//" $INPUT_FILE3 >tmp && mv tmp $INPUT_FILE3 && echo "Fixed $INPUT_FILE3"
 
