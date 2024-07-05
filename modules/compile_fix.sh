c190484ffcee357023c8e1b0bb977f29e49b85a7
Ninja Left <ninja.notleft@proton.me>
2024-07-05 15:04:52 +0000

    Added a 'design' prefix to files
    Signed-off-by: Ninja Left <ninja.notleft@proton.me>

--- modules/compile_fix.sh
+++ modules/compile_fix.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-INPUT_FILE='main_ui.py'
-INPUT_FILE2='bf_ui.py'
-INPUT_FILE3='config_ui.py'
+PREFIX="design"
+INPUT_FILE="$PREFIX/main_ui.py"
+INPUT_FILE2="$PREFIX/bf_ui.py"
+INPUT_FILE3="$PREFIX/config_ui.py"
 
 sed "s/import resources_rc//" $INPUT_FILE >tmp && mv tmp $INPUT_FILE && echo "Fixed $INPUT_FILE"
 sed "s/import resources_rc//" $INPUT_FILE2 >tmp && mv tmp $INPUT_FILE2 && echo "Fixed $INPUT_FILE2"
